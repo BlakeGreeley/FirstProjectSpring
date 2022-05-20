bac4135b155c7e70c66ad9bb2c96c92edf7e34cf
Blake Greeley <98073180+BlakeGreeley@users.noreply.github.com>
2022-05-19 19:43:15 -0700

    Add files via upload

---
+++ FirstprojectApplication1.java
@@ -0,0 +1,11 @@
+package com.codingdojo.firstproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class FirstprojectApplication1 {
+	public static void main(String[] args) {
+		SpringApplication.run(FirstprojectApplication.class, args);
+	}
+	
+}
